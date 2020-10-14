2e6b9fec2de485697f14b5d52c75d78de5b53d61
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2020-10-14 17:23:54 +0400

    fix: sort

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  let arr = [];
+  function towelSort (matrix) {
+    let arr = [];
     if(matrix.length>0)
     {
     for(let i in matrix)
     {
         for(let j in matrix[i])
+        if(i%2==0)
         arr.push(matrix[i][j]);
+        else arr.push(matrix[i][matrix[i].length-j-1])
     }
-    arr.sort(function (a,b) { return a-b;});
+    
     }
     return arr;
 }
+
