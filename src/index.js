f20298c2501c2a387bd949e7f1b8254a0d56024b
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2020-10-14 17:16:24 +0400

    feat: sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = [];
+    if(matrix.length>0)
+    {
+    for(let i in matrix)
+    {
+        for(let j in matrix[i])
+        arr.push(matrix[i][j]);
+    }
+    arr.sort(function (a,b) { return a-b;});
+    }
+    return arr;
 }
